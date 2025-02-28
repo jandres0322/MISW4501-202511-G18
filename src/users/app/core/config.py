ff72e98964977b8a91441ec444362638039d5c0e
j.garcia55 <j.garcia55@uniandes.edu.co>
2025-02-27 23:17:48 -0500

    Extension de expiración JWT

--- src/users/app/core/config.py
+++ src/users/app/core/config.py
@@ -5,4 +5,4 @@ class Config:
     SQLALCHEMY_DATABASE_URI = os.getenv('POSTGRES_DB_URI')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY')
-    JWT_ACCESS_TOKEN_EXPIRES = timedelta(minutes=15)
+    JWT_ACCESS_TOKEN_EXPIRES = timedelta(hours=1)
